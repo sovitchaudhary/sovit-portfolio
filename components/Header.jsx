5ad9b57152e5d46ddf24600f19e11c1c919ebb48
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-04 13:19:47 +0530

    Added logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,9 +4,15 @@ import { Button } from "./ui/button";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">
-            header
-        </div>
+      <div className="container mx-auto">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            SOVIT<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* Desktop Nav */}
+      </div>
     </header>
   );
 };

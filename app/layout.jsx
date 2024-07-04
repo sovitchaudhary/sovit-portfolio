9e02e3bc5d0eb7947a2c337c5e062542f8d5a51a
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-04 13:12:28 +0530

    create and import header component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
-const JetbrainsMono = JetBrains_Mono({ subsets: ["latin"],
+const JetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: '--font-jetbrainsMono'
- });
+  variable: "--font-jetbrainsMono",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -14,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={JetbrainsMono.variable}>{children}</body>
-    </html> 
+      <body className={JetbrainsMono.variable}>
+        <Header />
+        {children}
+      </body>
+    </html>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto">
+            header
+        </div>
+    </header>
+  );
+};
+
+export default Header;

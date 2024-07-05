ce6c19bffba4606bf7a70817403d4a1984a85845
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-06 01:19:14 +0530

    Add  MobileNav Page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -22,7 +24,9 @@ const Header = () => {
         </div>
 
         {/* mobile nav */}
-        <div className="xl:hidden">mobile nav</div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
---
+++ components/MobileNav.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const MobileNav = () => {
+  return (
+    <nav>MobileNav</nav>
+  )
+}
+
+export default MobileNav

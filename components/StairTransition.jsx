6c0f77e7bfcd7f197404f9d05c48f47998e7f655
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-06 01:11:55 +0530

    minor change

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -17,7 +17,7 @@ const StairTransition = () => {
           <motion.div
             className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
             initial={{ opacity: 1 }}
-            animate={{ opacity: 0, transition: { delay: 1, duration: 0.4, ease: "ease" } }}
+            animate={{ opacity: 0, transition: { delay: 0.8, duration: 0, ease: "ease" } }}
           />
         </div>
       </AnimatePresence>
